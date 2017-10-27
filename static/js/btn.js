382d9f7d70c703da36c1014697f62b3232f52f27
wylok <wylok@126.com>
2017-10-27 17:27:10 +0800

    Delete btn.js

--- static/js/btn.js
+++
@@ -1,7 +0,0 @@
-function btn(){
-    document.write('<link rel="stylesheet" type="text/css" href="/static/css/loading.css" /><div class="alpha"><div class="spinner"></div><div style="text-align:center;font-weight: bold">该操作执行中,请耐心等待......</div></div>');
-    document.onreadystatechange = completeLoading
-    if(document.readyState == "complete"){
-        document.close();
-    };
-}
