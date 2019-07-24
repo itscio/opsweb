302985d66639feedc7c710930baa7eb6db060915
wylok <wylok@126.com>
2019-07-24 14:51:39 +0800

    Delete __init__.py

--- admin/__init__.py
+++
@@ -1,6 +0,0 @@
-#-*- coding: utf-8 -*-
-import platform
-from Modules import loging
-logging = loging.Error()
-if platform.python_version().startswith('2.7.'):
-    logging.error("Python %s is not supported!" %platform.python_version())
